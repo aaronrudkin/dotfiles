87067702df5bdfce371e656ab8d8bffddd51e111
Aaron Rudkin <junk_signups@rudkin.ca>
2017-03-19 23:47:41 -0700

    Commenting install script.

--- install.sh
+++ install.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+# Setup symlinks
 ln -sF ~/.dotfiles/.zshrc ~/.zshrc
 ln -sF ~/.dotfiles/.hushlogin ~/.hushlogin
 ln -sF ~/.dotfiles/.nanorc ~/.nanorc
 
+# Decrypt encrypted stuff
 cd ~/.dotfiles
 ./decrypt.sh
+
+# Setup more symlinks
 ln -sF ~/.dotfiles/config ~/.ssh/config
 ln -sF ~/.dotfiles/.gitconfig ~/.gitconfig
+
+# Deploy the bin file.
 mkdir ~/bin
 ln -sF ~/.dotfiles/writecheck ~/bin/writecheck
