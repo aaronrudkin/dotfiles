e214dc18a0ab3ea50626631f5972442b2a539424
Aaron Rudkin <junk_signups@rudkin.ca>
2017-10-06 00:07:00 -0700

    Encrypted versions of SSH keys.

--- crypt.sh
+++ crypt.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 gpg --cipher-algo AES256 --symmetric -c config
 gpg --cipher-algo AES256 --symmetric -c .gitconfig
+gpg --cipher-algo AES256 --symmetric -c id_rsa
+gpg --cipher-algo AES256 --symmetric -c id_rsa.pub
+
--- decrypt.sh
+++ decrypt.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 gpg --output config --decrypt config.gpg
 gpg --output .gitconfig --decrypt .gitconfig.gpg
+gpg --output id_rsa --decrypt id_rsa.gpg
+gpg --output id_rsa.pub --decrypt id_rsa.pub.gpg
