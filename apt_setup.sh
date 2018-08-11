31d245608590149cef0ae0e4840d82201dcb7cef
Aaron Rudkin <aaronrudkin@users.noreply.github.com>
2018-08-10 21:03:19 -0700

    Update apt_setup.sh

--- apt_setup.sh
+++ apt_setup.sh
@@ -1,4 +1,4 @@
 echo "Apt setup"
 sudo apt-get update && apt-get upgrade
-sudo apt-get -y install zsh make python python-pip git unzip zip git-lfs spell xclip ncdu htop silversearcher-ag
-sudo apt-get -y install mediainfo imagemagick members pastebinit optipng jpegoptim cloc tree python python-pip jq
+sudo apt-get -y install zsh make python python-pip git unzip zip ncdu htop silversearcher-ag
+sudo apt-get -y install mediainfo imagemagick members pastebinit optipng jpegoptim cloc tree jq
